27a3a314cca934a4337a0c30f463a27ec0f20505
Beisenbek Baisakov <beysenbek@gmail.com>
2015-03-28 17:31:03 -0700

    pattern added

---
+++ RegexGroupSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexGroupSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = " abcd defg 123456";
+            string pattern = "^\\s(?<fs>[a-zA-Z]*)\\s(?<ss>[a-zA-Z]*)\\s(?<n>[0-9]{6})$";
+            Regex r = new Regex(pattern);
+
+            Match m = r.Match(test);
+
+            Console.WriteLine(m.Groups["fs"] + ":" + m.Groups["ss"] + ":" + m.Groups["n"]);
+                
+            
+        }
+    }
+}
